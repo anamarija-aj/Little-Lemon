ab1162d9e53ba21b752c682e762f861c3daa48db
AnamarijaJ <anamarija.jukic97@gmail.com>
2023-07-06 23:41:06 +0200

    Initial Commit

---
+++ Little Lemon/Little_LemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  Little_LemonApp.swift
+//  Little Lemon
+//
+//  Created by Anamarija Jukic on 06.07.2023..
+//
+
+import SwiftUI
+
+@main
+struct Little_LemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
