e98e1d38e27a13255c06d3a11820f320937f34b2
anamarija-aj <jukic.anamarija18@gmail.com>
2023-07-07 11:56:20 +0200

    Step 1: Adding a new home screen

---
+++ Little Lemon/Home.swift
@@ -0,0 +1,22 @@
+//
+//  Home.swift
+//  Little Lemon
+//
+//  Created by Anamarija Jukic on 07.07.2023..
+//
+
+import SwiftUI
+
+struct Home: View {
+    var body: some View {
+        TabView {
+            
+        }
+    }
+}
+
+struct Home_Previews: PreviewProvider {
+    static var previews: some View {
+        Home()
+    }
+}
