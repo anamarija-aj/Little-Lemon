89c914f67c41ba625d6714f344406ff7c3f65736
anamarija-aj <jukic.anamarija18@gmail.com>
2023-07-07 12:00:52 +0200

    Step 2: Adding a new Menu screen to hold the menu information

---
+++ Little Lemon/Menu.swift
@@ -0,0 +1,28 @@
+//
+//  Menu.swift
+//  Little Lemon
+//
+//  Created by Anamarija Jukic on 07.07.2023..
+//
+
+import SwiftUI
+
+struct Menu: View {
+    var body: some View {
+        VStack {
+            Text("Little Lemon")
+            Text("Chicago")
+            Text("We are a family owned Mediterranean restaurant, focused on traditional recipes served with a modern twist.")
+            
+            List {
+                
+            }
+        }
+    }
+}
+
+struct Menu_Previews: PreviewProvider {
+    static var previews: some View {
+        Menu()
+    }
+}
