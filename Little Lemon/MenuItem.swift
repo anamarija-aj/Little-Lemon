fb09e6cb30d77ba2b89649ba1f16d488f63ce19e
anamarija-aj <jukic.anamarija18@gmail.com>
2023-07-07 14:48:55 +0200

    Step 1: Creating menu models

---
+++ Little Lemon/MenuItem.swift
@@ -0,0 +1,14 @@
+//
+//  MenuItem.swift
+//  Little Lemon
+//
+//  Created by Anamarija Jukic on 07.07.2023..
+//
+
+import Foundation
+
+struct MenuItem: Decodable {
+    let title: String
+    let image: String
+    let price: String
+}
---
+++ Little Lemon/MenuList.swift
@@ -0,0 +1,12 @@
+//
+//  MenuList.swift
+//  Little Lemon
+//
+//  Created by Anamarija Jukic on 07.07.2023..
+//
+
+import Foundation
+
+struct MenuList: Decodable {
+    let menu: [MenuItem]
+}
