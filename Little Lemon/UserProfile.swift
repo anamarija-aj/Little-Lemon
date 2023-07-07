5208a267fe196f7c8d911df55f8c4ea11723c630
anamarija-aj <jukic.anamarija18@gmail.com>
2023-07-07 12:23:36 +0200

    Step 1: Create a new SwiftUI file for onboarding.

---
+++ Little Lemon/UserProfile.swift
@@ -0,0 +1,20 @@
+//
+//  UserProfile.swift
+//  Little Lemon
+//
+//  Created by Anamarija Jukic on 07.07.2023..
+//
+
+import SwiftUI
+
+struct UserProfile: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct UserProfile_Previews: PreviewProvider {
+    static var previews: some View {
+        UserProfile()
+    }
+}
