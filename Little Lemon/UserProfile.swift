62c946d4397dcfd9feefa86667387672b7e867f3
anamarija-aj <jukic.anamarija18@gmail.com>
2023-07-08 18:13:38 +0200

    add scroll view

--- Little Lemon/UserProfile.swift
+++ Little Lemon/UserProfile.swift
@@ -21,87 +21,89 @@ struct UserProfile: View {
     var body: some View {
         
         NavigationView {
-            VStack {
-                Text("Personal information")
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                    .padding([.leading, .trailing, .top], 20)
-                    .foregroundColor(Color(red: 51/255, green: 51/255, blue: 51/255))
-                Text("Avatar")
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                    .padding([.leading, .trailing, .top], 20)
-                    .foregroundColor(Color(red: 51/255, green: 51/255, blue: 51/255))
-                
-                HStack {
-                    Image("profile-image-placeholder")
-                        .resizable()
-                        .aspectRatio(contentMode: .fill)
-                        .frame(width: 100, height: 100, alignment: .center)
-                        .clipped()
+            ScrollView {
+                VStack {
+                    Text("Personal information")
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .padding([.leading, .trailing, .top], 20)
+                        .foregroundColor(Color(red: 51/255, green: 51/255, blue: 51/255))
+                    Text("Avatar")
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .padding([.leading, .trailing, .top], 20)
+                        .foregroundColor(Color(red: 51/255, green: 51/255, blue: 51/255))
                     
-                    Button(action: { /* action for each category */ }) {
-                        Text("Change")
-                            .padding(EdgeInsets(top: 9, leading: 14, bottom: 9, trailing: 14))
-                            .background(Color(red: 73/255, green: 94/255, blue: 87/255))
-                            .foregroundColor(Color(red: 237/255, green: 239/255, blue: 238/255))
-                            .cornerRadius(10)
-                    }
-                    .padding()
-                    
-                    Button(action: { /* action for each category */ }) {
-                        Text("Remove")
-                            .padding(EdgeInsets(top: 9, leading: 14, bottom: 9, trailing: 14))
-                            .background(.white)
-                            .foregroundColor(Color(red: 73/255, green: 94/255, blue: 87/255))
+                    HStack {
+                        Image("profile-image-placeholder")
+                            .resizable()
+                            .aspectRatio(contentMode: .fill)
+                            .frame(width: 100, height: 100, alignment: .center)
+                            .clipped()
                         
-                    }
-                    .overlay(
-                        RoundedRectangle(cornerRadius: 0) // Adjust corner radius if needed
-                            .stroke(Color(red: 73/255, green: 94/255, blue: 87/255), lineWidth: 1)
-                    )
-                }
-                
-                
-                Text("First Name")
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                    .padding([.leading, .trailing, .top], 20)
-                    .foregroundColor(Color(red: 51/255, green: 51/255, blue: 51/255))
-                Text(firstName)
-                
-                
-                Text("Last Name")
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                    .padding([.leading, .trailing, .top], 20)
-                    .foregroundColor(Color(red: 51/255, green: 51/255, blue: 51/255))
-                Text(lastName)
-                
-                
-                Text("Email")
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                    .padding([.leading, .trailing, .top], 20)
-                    .foregroundColor(Color(red: 51/255, green: 51/255, blue: 51/255))
-                Text(email)
-                
-                Button {
-                    UserDefaults.standard.set(false, forKey: "kIsLoggedIn")
-                    self.showOnboarding = true
-                    self.presentation.wrappedValue.dismiss()
-                    
-                } label: {
-                    Text("Logout")
-                        .foregroundColor(.black)
-                        .frame(maxWidth: .infinity)
+                        Button(action: { /* action for each category */ }) {
+                            Text("Change")
+                                .padding(EdgeInsets(top: 9, leading: 14, bottom: 9, trailing: 14))
+                                .background(Color(red: 73/255, green: 94/255, blue: 87/255))
+                                .foregroundColor(Color(red: 237/255, green: 239/255, blue: 238/255))
+                                .cornerRadius(10)
+                        }
                         .padding()
-                        .background(Color(red: 244/255, green: 206/255, blue: 20/255))
-                        .cornerRadius(5)
+                        
+                        Button(action: { /* action for each category */ }) {
+                            Text("Remove")
+                                .padding(EdgeInsets(top: 9, leading: 14, bottom: 9, trailing: 14))
+                                .background(Color.white)
+                                .foregroundColor(Color(red: 73/255, green: 94/255, blue: 87/255))
+                            
+                        }
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 0) // Adjust corner radius if needed
+                                .stroke(Color(red: 73/255, green: 94/255, blue: 87/255), lineWidth: 1)
+                        )
+                    }
+                    
+                    
+                    Text("First Name")
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .padding([.leading, .trailing, .top], 20)
+                        .foregroundColor(Color(red: 51/255, green: 51/255, blue: 51/255))
+                    Text(firstName)
+                    
+                    
+                    Text("Last Name")
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .padding([.leading, .trailing, .top], 20)
+                        .foregroundColor(Color(red: 51/255, green: 51/255, blue: 51/255))
+                    Text(lastName)
+                    
+                    
+                    Text("Email")
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .padding([.leading, .trailing, .top], 20)
+                        .foregroundColor(Color(red: 51/255, green: 51/255, blue: 51/255))
+                    Text(email)
+                    
+                    Button {
+                        UserDefaults.standard.set(false, forKey: "kIsLoggedIn")
+                        self.showOnboarding = true
+                        self.presentation.wrappedValue.dismiss()
+                        
+                    } label: {
+                        Text("Logout")
+                            .foregroundColor(.black)
+                            .frame(maxWidth: .infinity)
+                            .padding()
+                            .background(Color(red: 244/255, green: 206/255, blue: 20/255))
+                            .cornerRadius(5)
+                    }
+                    .padding([.leading, .trailing])
+                    .fullScreenCover(isPresented: $showOnboarding) {
+                        Onboarding()
+                    }
+                    
+                    
+                    
+                    
                 }
-                .padding([.leading, .trailing])
-                .fullScreenCover(isPresented: $showOnboarding) {
-                    Onboarding()
-                }
-                
-                
-                
-                
             }
         }
         
